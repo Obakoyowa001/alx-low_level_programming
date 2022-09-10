c8a8de7bf16bc4924600f5af3e15bac6f141e0d4
obakoyowa001 <ayobstheheadboy21@gmail.com>
2022-09-09 18:53:15 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,6 +6,6 @@ int main(void)
  * Return: Always 0 (success)
  */
 {
-	puts("\"programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
