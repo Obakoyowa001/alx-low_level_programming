9f75087604a97f3ac2733461b5d5ccf4e8fdc2c1
obakoyowa001 <ayobstheheadboy21@gmail.com>
2022-09-14 17:06:28 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always0 (success)
+ */
+int main(void)
+{
+	int i =0;
+	char text[] = "_putchar";
+
+	while (text[i]);
+	{
+		_putchar(text[i]);
+		i++;
+	}
+	_putchar('\n')
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <"main.h">
+/**
+ * main - Entry point
+ * 
+ * Return - always (0) (success)
+ */
+void print_alphabet(void)
+{
+	char c;
+	
+	for ( c = a ; c < z ; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
