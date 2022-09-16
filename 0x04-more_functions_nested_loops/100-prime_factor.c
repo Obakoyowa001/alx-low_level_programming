b99118fd2b7c32f2ea71bab0aae88842164f0c2b
obakoyowa001 <ayobstheheadboy21@gmail.com>
2022-09-15 20:18:26 -0700

    11&12

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -8,8 +8,8 @@ int main(void)
 {
 	unsigned long int i, n;
 
-	n = 612852475143
-		for (i = 3; i < 782849; i = i + 2)
+	n = 612852475143;
+	for (i = 3; i < 782849; i = i + 2)
 		{
 			while ((n % i == 0) && (n != i))
 			{
