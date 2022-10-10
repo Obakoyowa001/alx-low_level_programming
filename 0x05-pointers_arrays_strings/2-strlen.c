0d0c105aec6bb7fef58a19f315f96450861bbf70
obakoyowa001 <ayobstheheadboy21@gmail.com>
2022-10-10 06:17:03 -0700

    late project

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+/**
+ * int _strlen(char *s) - a function that returns the length of a string.
+ *
+ * @s: parameter 1
+ *
+ * Return: The lenght of a string
+ */
+
+int _strlen(char *s);
+{
+	char string[] = "Hello" ;
+	int len;
+
+	strlen(string);
+	printf("strlen (string) is %d\n" string);
+
+	return (len);
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+this is the resdme for strings pointers and array
